d89b83eb8ea01a72035f869f8b6e64eaaed315f9
Michel Farah <Michelfarahh7@gmail.com>
2023-10-24 23:26:10 -0400

    finalized first try incl. Java

--- app/src/main/java/com/example/minicapstone/SettingsActivity.java
+++ app/src/main/java/com/example/minicapstone/SettingsActivity.java
@@ -1,4 +1,43 @@
 package com.example.minicapstone;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class SettingsActivity {
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        //Initializing action bar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        setTitle("Settings");
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
