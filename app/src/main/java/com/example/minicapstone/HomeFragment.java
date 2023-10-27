00858c0b7d2b374d5b2a8d0536a63058e5b3cc06
g305tmp <g305tmp@gmail.com>
2023-10-27 03:10:27 -0400

    Added video feed in Home Page + Added back-end for video feed in HomeFragment.java + Added placeholder front-end in fragment_home.xml

--- app/src/main/java/com/example/minicapstone/HomeFragment.java
+++ app/src/main/java/com/example/minicapstone/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.minicapstone;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,11 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -16,15 +22,27 @@ public class HomeFragment extends Fragment {
     //Class Attributes
     View view;
 
+    //Views
+    private WebView webViewVideoFeed;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_home, container, false);
 
         //Show Toolbar
         ((MainActivity)getActivity()).getSupportActionBar().show();
 
+        //Retrieve View ID's
+        webViewVideoFeed = view.findViewById(R.id.webViewVideoFeed);
+
+        //Setup WebView
+        webViewVideoFeed.setWebViewClient(new WebViewClient());
+        webViewVideoFeed.loadUrl("https://8.8.8.8");
+        WebSettings webSettings=webViewVideoFeed.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
         return view;
     }
@@ -45,6 +63,19 @@ public class HomeFragment extends Fragment {
 
         //Hide Toolbar
         ((MainActivity)getActivity()).getSupportActionBar().show();
+    }
+
+
+    public class mywebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view,url,favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 
 }
