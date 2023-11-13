850fe0ace97da8c9d3e46f927749e981f919424e
Michel Farah <Michelfarahh7@gmail.com>
2023-11-13 15:00:30 -0500

    finished calculations require testing

--- app/src/main/java/com/example/minicapstone/HomeFragment.java
+++ app/src/main/java/com/example/minicapstone/HomeFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -42,8 +43,10 @@ public class HomeFragment extends Fragment {
 
     //Vectors to be later extracted from database
 
-    double[] U;
-    double[] V;
+    double[] uLeft = new double[2];
+    double[] vLeft = new double[2];
+    double[] uRight = new double[2];
+    double[] vRight = new double[2];
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -79,6 +82,70 @@ public class HomeFragment extends Fragment {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 // Handle errors
+            }
+        });
+
+        // Get a reference to the "pose_data" node
+        DatabaseReference poseDataRef = FirebaseDatabase.getInstance().getReference("pose_data");
+
+        poseDataRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Extract values from "vector_11_to_23"
+                DataSnapshot vectorLeft1Snap = dataSnapshot.child("vector_11_to_23");
+                double value0LeftVector1 = vectorLeft1Snap.child("0").getValue(Double.class);
+                double value1LeftVector1 = vectorLeft1Snap.child("1").getValue(Double.class);
+
+                // Extract values from "vector_23_to_25"
+                DataSnapshot vectorLeft2Snap = dataSnapshot.child("vector_23_to_25");
+                double value0LeftVector2 = vectorLeft2Snap.child("0").getValue(Double.class);
+                double value1LeftVector2 = vectorLeft2Snap.child("1").getValue(Double.class);
+
+                // Log values for vector_11_to_23
+                Log.d("TAG", "vector_11_to_23 - Value 0: " + value0LeftVector1 + ", Value 1: " + value1LeftVector1);
+
+                // Log values for vector_23_to_25
+                Log.d("TAG", "vector_23_to_25 - Value 0: " + value0LeftVector2 + ", Value 1: " + value1LeftVector2);
+
+
+                // Extract values from "vector_12_to_24"
+                DataSnapshot vectorRight1Snap = dataSnapshot.child("vector_12_to_24");
+                double value0RightVector1 = vectorRight1Snap.child("0").getValue(Double.class);
+                double value1RightVector1 = vectorRight1Snap.child("1").getValue(Double.class);
+
+                // Extract values from "vector_24_to_26"
+                DataSnapshot vectorRight2Snap = dataSnapshot.child("vector_24_to_26");
+                double value0RightVector2 = vectorRight2Snap.child("0").getValue(Double.class);
+                double value1RightVector2 = vectorRight2Snap.child("1").getValue(Double.class);
+
+                // Log values for vector_12_to_24
+                Log.d("TAG", "vector_12_to_24 - Value 0: " + value0RightVector1 + ", Value 1: " + value1RightVector1);
+
+                // Log values for vector_24_to_26
+                Log.d("TAG", "vector_24_to_26 - Value 0: " + value0RightVector2 + ", Value 1: " + value1RightVector2);
+
+
+
+                // Store values in U and V arrays for further calculations
+                uLeft[0] = value0LeftVector1;
+                uLeft[1] = value1LeftVector1;
+
+                vLeft[0] = value0LeftVector2;
+                vLeft[1] = value1LeftVector2;
+
+                uRight[0] = value0RightVector1;
+                uRight[1] = value1RightVector1;
+
+                vRight[0] = value0RightVector2;
+                vRight[1] = value1RightVector2;
+
+                // Call calculateAngle after updating U and V
+                calculateAngle();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("TAG", "Error: " + databaseError.getMessage());
             }
         });
 
@@ -125,34 +192,46 @@ public class HomeFragment extends Fragment {
 
     //Calculations on vectors to deduce if correct/wrong posture
     private void calculateAngle() {
+        // Calculate angle for the left vectors
+        double angleLeft = calculateAngleBetweenVectors(uLeft, vLeft);
+
+        // Calculate angle for the right vectors
+        double angleRight = calculateAngleBetweenVectors(uRight, vRight);
+
+        // Update the UI based on both left and right angles
+        if ((angleLeft > 85 && angleLeft < 110) || (angleRight > 85 && angleRight < 110)) {
+            // Update the UI if good posture
+            upperStatusText.setText("Good posture");
+            lowerStatusText.setText("Keep your back straight");
+            colorStatus.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.greencustom));
+        } else {
+            // Update the UI for other cases (e.g., bad posture)
+            upperStatusText.setText("Bad posture");
+            lowerStatusText.setText("Straighten your back");
+            colorStatus.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.orangeCustom3));
+        }
+    }
+
+    // Helper method to calculate angle between two vectors
+    private double calculateAngleBetweenVectors(double[] u, double[] v) {
         // Calculate dot product
-        double dotProduct = (U[0] * V[0]) + (U[1] * V[1]);
+        double dotProduct = (u[0] * v[0]) + (u[1] * v[1]);
 
         // Calculate magnitudes
-        double magnitudeU = Math.sqrt(U[0] * U[0] + U[1] * U[1]);
-        double magnitudeV = Math.sqrt(V[0] * V[0] + V[1] * V[1]);
+        double magnitudeU = Math.sqrt(u[0] * u[0] + u[1] * u[1]);
+        double magnitudeV = Math.sqrt(v[0] * v[0] + v[1] * v[1]);
 
         // Calculate cosine of the angle
         double cosineTheta = dotProduct / (magnitudeU * magnitudeV);
 
         // Calculate the angle in radians
-        double angleDegree = Math.acos(cosineTheta);
+        double angleRad = Math.acos(cosineTheta);
 
-        // Do something with the calculated angle if needed
-        // For example, display it in a TextView or perform further actions
+        // Convert radians to degrees
+        double angleDegree = Math.toDegrees(angleRad);
 
-        if(angleDegree > 85 && angleDegree < 110){
-
-            //Update the UI if good posture
-            upperStatusText.setText("Good posture");
-            lowerStatusText.setText("Keep your back straight");
-            colorStatus.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.greencustom));
-
-        }
+        return angleDegree;
     }
-
-
-
 
 
 
