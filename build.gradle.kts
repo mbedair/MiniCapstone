73118b3aca491a9241aaa0533eb168d7f545920c
Elias <eliashaddad114@gmail.com>
2023-11-22 17:47:31 -0500

    Fixed the issue where authentication was unusable on some of our devices:
    1- added "id("com.google.gms.google-services") version "4.4.0" apply false" to the program level gradle and removed the buildscript lines.
    2- used the firebase BOM to automatically select the suitable firebase library in the app level gradle.
    3- removed the digital fingerprint from the firebase console.
    4- added a line in the xml files for login and signup to set the edittext input type of the email address edittexts to textEmailAddress.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,7 @@
-buildscript {
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
-    }
 
-    repositories {
-        jcenter()
-    }
-}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.2" apply false
+
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
