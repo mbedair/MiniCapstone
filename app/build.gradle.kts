571204ddfdfda1a450f2765e2b6f3c9839d45876
Omar-Abouelatta <omaraymangalal@gmail.com>
2023-11-27 23:38:16 -0500

    added pie chart demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,8 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
 
+    implementation("com.github.AnyChart:AnyChart-Android:1.1.5")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -44,4 +46,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
 }
